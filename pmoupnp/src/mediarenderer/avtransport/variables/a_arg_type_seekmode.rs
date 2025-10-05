7e193fb2300cf9d6d3de2fc5a0d20d7109f1d12a
Eric Coissac <eric@coissac.eu>
2025-10-05 20:34:04 +0200

    Je laisse Claude faire ses modification sur avtransport

--- pmoupnp/src/mediarenderer/avtransport/variables/a_arg_type_seekmode.rs
+++ pmoupnp/src/mediarenderer/avtransport/variables/a_arg_type_seekmode.rs
@@ -5,13 +5,13 @@ use crate::variable_types::{StateValue, StateVarType};
 use once_cell::sync::Lazy;
 
 pub static A_ARG_TYPE_SEEKMODE: Lazy<Arc<StateVariable>> = Lazy::new(|| -> Arc<StateVariable> {
-    let mut sv = StateVariable::new(StateVarType::UI4, "A_ARG_TYPE_SeekMode".to_string());
+    let mut sv = StateVariable::new(StateVarType::String, "A_ARG_TYPE_SeekMode".to_string());
 
-    sv.extend_allowed_values(&[ 
+    sv.extend_allowed_values(&[
         StateValue::String("TRACK_NR".to_string()),
 		StateValue::String("REL_TIME".to_string()),
 		StateValue::String("ABS_TIME".to_string()),
-        ]).expect("Cannot set default value");
+        ]).expect("Cannot set allowed values");
 
     Arc::new(sv)
 });
