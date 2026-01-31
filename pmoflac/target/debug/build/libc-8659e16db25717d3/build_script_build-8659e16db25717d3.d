90c8a6e5628f18c3fcc3c796c5101055496704f1
Eric Coissac <eric@coissac.eu>
2026-01-31 12:19:02 +0100

    Un peu de ménage pour retirer les répertoires target poussés par erreur.

--- pmoflac/target/debug/build/libc-8659e16db25717d3/build_script_build-8659e16db25717d3.d
+++
@@ -1,5 +0,0 @@
-/Users/coissac/Sync/maison/Petite_maisons/src/pmomusic/pmoflac/target/debug/build/libc-8659e16db25717d3/build_script_build-8659e16db25717d3.d: /Users/coissac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.177/build.rs
-
-/Users/coissac/Sync/maison/Petite_maisons/src/pmomusic/pmoflac/target/debug/build/libc-8659e16db25717d3/build_script_build-8659e16db25717d3: /Users/coissac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.177/build.rs
-
-/Users/coissac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.177/build.rs:
--- pmoflac/target/debug/deps/flac_streams-c03d377b25a30dad.d
+++
@@ -1,7 +0,0 @@
-/Users/coissac/Sync/maison/Petite_maisons/src/pmomusic/pmoflac/target/debug/deps/flac_streams-c03d377b25a30dad.d: tests/flac_streams.rs
-
-/Users/coissac/Sync/maison/Petite_maisons/src/pmomusic/pmoflac/target/debug/deps/flac_streams-c03d377b25a30dad: tests/flac_streams.rs
-
-tests/flac_streams.rs:
-
-# env-dep:CARGO_MANIFEST_DIR=/Users/coissac/Sync/maison/Petite_maisons/src/pmomusic/pmoflac
